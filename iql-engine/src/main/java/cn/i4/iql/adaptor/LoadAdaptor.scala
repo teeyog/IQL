3871aa522bf49601720c9b8d264824bbbd81a01d
SOBIGUFO <teeyog@gmail.com>
2018-04-16 17:58:44 +0800

    kafka format

--- iql-engine/src/main/java/cn/i4/iql/adaptor/LoadAdaptor.scala
+++ iql-engine/src/main/java/cn/i4/iql/adaptor/LoadAdaptor.scala
@@ -54,6 +54,7 @@ class LoadAdaptor(scriptSQLExecListener: IQLSQLExecListener) extends DslAdaptor 
         reader.option("zookeeper.connect",option.getOrElse("zookeeper.connect","kafka.zookeeper.connect"))
         reader.option("topics",path)
         table = reader.format("org.apache.spark.sql.execution.datasources.kafka").load()
+        table = scriptSQLExecListener.sparkSession.read.json(table.select("msg").rdd.map(_.getString(0)))
 
       case "json" | "csv" | "orc" | "parquet" | "text" =>
         if(path.startsWith("'") || path.startsWith("`") || path.startsWith("\"")) path = path.substring(1)
