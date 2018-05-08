c332c4f83f4f447c373681b64ab8678da7cfc209
SOBIGUFO <teeyog@gmail.com>
2018-05-08 10:25:58 +0800

    iql

--- iql-springboot-http/src/main/java/cn/i4/iql/http/service/primary/IqlExcutionRepository.java
+++ iql-springboot-http/src/main/java/cn/i4/iql/http/service/primary/IqlExcutionRepository.java
@@ -1,11 +1,7 @@
 package cn.i4.iql.http.service.primary;
 
 import cn.i4.iql.http.bean.primary.IqlExcution;
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.sql.Timestamp;
 
 public interface IqlExcutionRepository  extends JpaRepository<IqlExcution, Long> {
 
