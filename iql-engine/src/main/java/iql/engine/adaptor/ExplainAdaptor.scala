84ac48fd6834e5430d41f8e47c955830a9622e15
SOBIGUFO <teeyog@gmail.com>
2018-12-06 11:19:23 +0800

    add explain grammar

---
+++ iql-engine/src/main/java/iql/engine/adaptor/ExplainAdaptor.scala
@@ -0,0 +1,32 @@
+package iql.engine.adaptor
+
+import java.util.UUID
+
+import iql.engine.IQLSQLExecListener
+import iql.engine.antlr.IQLLexer
+import iql.engine.antlr.IQLParser.SqlContext
+import org.antlr.v4.runtime.misc.Interval
+
+class ExplainAdaptor(scriptSQLExecListener: IQLSQLExecListener) extends DslAdaptor {
+    override def parse(ctx: SqlContext): Unit = {
+        val sparkSession = scriptSQLExecListener.sparkSession
+        val input = ctx.start.getTokenSource.asInstanceOf[IQLLexer]._input
+        val start = ctx.start.getStartIndex
+        val stop = ctx.stop.getStopIndex
+        val interval = new Interval(start, stop)
+        val originalText = input.getText(interval)
+        var tableName = ""
+
+        val chunks = originalText.replace(";", "").split("\\s+")
+        chunks.length match {
+            case 2 =>
+                scriptSQLExecListener.addResult("explainStr", sparkSession.table(chunks(1)).queryExecution.toString())
+            case _ =>
+                val uuidTable = UUID.randomUUID().toString.replace("-", "")
+                scriptSQLExecListener.addResult("uuidTable", uuidTable)
+                sparkSession.sql(originalText).createOrReplaceTempView(uuidTable)
+        }
+
+        if (chunks(chunks.length - 2).equals("as")) tableName = chunks.last.replace(";", "")
+    }
+}
