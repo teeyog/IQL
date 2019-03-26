7f470f7b4940b3178c1dde3b2f7b43f0fbc9043e
ufo <ufo@sinaif.com>
2019-03-26 10:07:03 +0800

    update default udf

--- iql-engine/src/main/java/iql/engine/utils/SparkUDF.scala
+++ iql-engine/src/main/java/iql/engine/utils/SparkUDF.scala
@@ -1,27 +1,11 @@
 package iql.engine.utils
 
-import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.sql.SparkSession
 
 /**
  * 常用Spark UDF
  */
 object SparkUDF {
-
-  /**
-   * 过滤非数字字符串
-   */
-  def filterNumStr(spark:SparkSession) = {
-    spark.udf.register("filterNumStr",(s: String) => {
-      if(s.indexOf("pro-")>=0){
-        s.trim
-      } else if (s == null || (!s.matches("^[0-9]+$")) || s.length>10) {
-        "-1"
-      } else {
-        s.trim
-      }
-    })
-  }
 
   /**
     * 根据正则过滤字符串
@@ -36,19 +20,6 @@ object SparkUDF {
       }
     })
   }
-
-  /**
-   * 格式化model
-   */
-  def getModel(spark:SparkSession) = {
-    spark.udf.register("getModel",(a:String) => {
-      if(a.toLowerCase.startsWith("ipho")) 1
-      else if(a.toLowerCase.startsWith("ipad")) 2
-      else 3
-    })
-  }
-
-
 
   /**
     * 字符串转数组
