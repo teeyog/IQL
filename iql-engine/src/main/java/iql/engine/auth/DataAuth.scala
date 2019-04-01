61ca4ff6b56a4af3373b694cf1dc1eed1def65fb
teeyog <teeyog@gmail.com>
2019-04-01 20:46:19 +0800

    fix auth bug

--- iql-engine/src/main/java/iql/engine/auth/DataAuth.scala
+++ iql-engine/src/main/java/iql/engine/auth/DataAuth.scala
@@ -7,27 +7,27 @@ import org.apache.http.client.fluent.{Form, Request}
 
 object DataAuth {
 
-    def auth(table: List[MLSQLTable]): List[TableAuthResult] = {
-        //构造jsonArray
-        val tableArray = new JSONArray()
-        table.filter(_.tableType.name != "temp").foreach(t => {
-            tableArray.add(ObjGenerator.newJSON(Seq(("type", t.tableType.name), ("db", t.db.getOrElse("")), ("table", t.table.getOrElse(""))): _*))
-        })
-        //请求
-        val url = PropsUtils.get("iql.server.address") + "/auth/checkAuth"
-        val result = Request.Post(url).bodyForm(Form.form().add("data", tableArray.toJSONString).
-            add("token", PropsUtils.get("token"))
-            .build())
-            .execute().returnContent().asString()
+  def auth(table: List[MLSQLTable], token: String): List[TableAuthResult] = {
+    //构造jsonArray
+    val tableArray = new JSONArray()
+    table.filter(_.tableType.name != "temp").foreach(t => {
+      tableArray.add(ObjGenerator.newJSON(Seq(("type", t.tableType.name), ("db", t.db.getOrElse("")), ("table", t.table.getOrElse(""))): _*))
+    })
+    //请求
+    val url = PropsUtils.get("iql.server.address") + "/auth/checkAuth"
+    val result = Request.Post(url).bodyForm(Form.form().add("data", tableArray.toJSONString).
+      add("token", token)
+      .build())
+      .execute().returnContent().asString()
 
-        //结果映射
-        var tar: List[TableAuthResult] = List[TableAuthResult]()
-        val iter = JSON.parseArray(result).iterator()
-        while (iter.hasNext) {
-            val obj = JSON.parseObject(iter.next().toString)
-            tar = List(TableAuthResult(obj.getBooleanValue("granted"), obj.getString("msg"))) ::: tar
-        }
-        tar
+    //结果映射
+    var tar: List[TableAuthResult] = List[TableAuthResult]()
+    val iter = JSON.parseArray(result).iterator()
+    while (iter.hasNext) {
+      val obj = JSON.parseObject(iter.next().toString)
+      tar = List(TableAuthResult(obj.getBooleanValue("granted"), obj.getString("msg"))) ::: tar
     }
+    tar
+  }
 
 }
