ac32d60ee572b065b1e86a672f7ba2ddb1c117f1
teeyog <teeyog@gmail.com>
2019-04-11 22:50:50 +0800

    update config

--- iql-engine/src/main/java/iql/engine/config/config.scala
+++ iql-engine/src/main/java/iql/engine/config/config.scala
@@ -2,32 +2,32 @@ package iql.engine
 
 package object config {
 
-	val INIT_HIVE_CATALOG = ConfigBuilder("iql.hiveCatalog.init.enable")
+	val INIT_HIVE_CATALOG = ConfigBuilder("spark.iql.hiveCatalog.init.enable")
 		.doc("是否获取hive元数据显示在web端")
 		.booleanConf
 		.createWithDefault(true)
 
-	val HIVE_CATALOG_AUTO_COMPLETE = ConfigBuilder("iql.hiveCatalog.autoComplete")
+	val HIVE_CATALOG_AUTO_COMPLETE = ConfigBuilder("spark.iql.hiveCatalog.autoComplete")
 		.doc("SQL编辑框是否获取hive元数据以自动补全")
 		.booleanConf
 		.createWithDefault(true)
 
-	val IQL_AUTH_ENABLE = ConfigBuilder("iql.auth.enable")
+	val IQL_AUTH_ENABLE = ConfigBuilder("spark.iql.auth.enable")
 		.doc("是否开启权限验证")
 		.booleanConf
 		.createWithDefault(false)
 
-	val IQL_PARALLELISM = ConfigBuilder("iql.parallelism")
+	val IQL_PARALLELISM = ConfigBuilder("spark.iql.parallelism")
 		.doc("iql并行度")
 		.intConf
 		.createWithDefault(3)
 
-	val MAIL_ENABLE = ConfigBuilder("mail.enable")
+	val MAIL_ENABLE = ConfigBuilder("spark.mail.enable")
 		.doc("是否启用邮件通知")
 		.booleanConf
 		.createWithDefault(true)
 
-	val STREAMJOB_MAXATTRPTS = ConfigBuilder("streamJobMaxAttempts")
+	val STREAMJOB_MAXATTRPTS = ConfigBuilder("spark.streamJobMaxAttempts")
 		.doc("实时任务失败重启次数")
 		.intConf
 		.createWithDefault(3)
